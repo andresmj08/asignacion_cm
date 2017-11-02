2b65dd03e86a6c5ea4779ec9864fbb16472c4c64
edwing93 <33108263+edwing93@users.noreply.github.com>
2017-11-01 19:28:13 -0500

    Edwin Gonzalez
    Bboton regresar del menu registrar

--- application/views/cliente/registrar.php
+++ application/views/cliente/registrar.php
@@ -20,6 +20,8 @@
 <body>
  <body>
   
+
+<form action="agendar" method="post">
       <div class="container">    
         <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
             <div class="panel panel-info" >
@@ -31,7 +33,7 @@
 
                         <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
                             
-                        <form id="loginform" class="form-horizontal" role="form" method="post" action="validar">
+<form id="loginform" class="form-horizontal" role="form" method="post" action="validar">
                                     
                             <div style="margin-bottom: 25px" class="input-group">
                                         <span class="input-group-addon"><i class="glyphicon glyphicon-cog"></i></span>
@@ -81,14 +83,18 @@
                                 <div style="margin-top:10px" class="form-group">
                                     <!-- Button -->
 
-                                    <div class="col-sm-12 controls">
+                                  <div class="col-sm-12 controls">
                                       <button class="btn btn-success btn-lg btn-block">Registrarse</button>
-                                    </div>
+                                    </div><br>
                                    
-                                </div>  
-                                
+                                                        </div><br>
+        
                             </form>     
-
+<form action="<?=base_url()?>index.php/Op_cliente">
+	<div class="col-sm-12 controls">
+		<input type="submit" id="volver" class="btn btn-success btn-lg btn-block" value="Regresar"></input>
+	</div>
+</form>
 
 
                         </div>                     
